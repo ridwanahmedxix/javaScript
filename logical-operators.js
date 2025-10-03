0221f4045409d5b7b6185d236b5d0d08ea2ffd80
ridwancodexr <ridwancodexr@gmail.com>
2025-10-03 14:58:29 +0600

    add

---
+++ logical-operators.js
@@ -0,0 +1,11 @@
+let sscPassed = true ;
+let gpa = 4 ;
+let knowBangla = true ;
+let knowEnglish = false ;
+
+if( sscPassed === true && gpa >=2 && knowBangla === true || knowEnglish === false  ){
+    console.log(" ami collage porte parbo ");
+}
+else{
+    console.log(" ami collage porte parbo ");
+}
