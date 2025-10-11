e0996ab0ec9b0d6f46cf322a98f362bcc95bf89d
ridwancodexr <ridwancodexr@gmail.com>
2025-10-11 13:59:06 +0600

    add

---
+++ function.js
@@ -0,0 +1,7 @@
+function myFunc( name , country ) {
+      console.log(" My name is " + name + "and i am from " + country);
+     
+}
+myFunc( "Ridwan " , "Bangladesh") ;
+myFunc( "Ava " , "USA") ;
+myFunc( "Shafin " , "UK") ;
