53d666428954a39ee48ef6fd8c20dcf6107dc8c1
ridwancodexr <ridwancodexr@gmail.com>
2025-10-12 08:25:21 +0600

    add

--- rest-operator.js
+++ rest-operator.js
@@ -1,5 +1,5 @@
 
-function addToCard (number) {
+function addToCard (num1,num2,num3, ...number) {
     return number
 }
-console.log(addToCard(100));
+console.log(addToCard(100,200,300,400,500,600));
---
+++ spread-operator.js
@@ -0,0 +1,6 @@
+
+let arr1 = [10 , 20 , 30] ;
+let arr2 = [40 , 50 , 60] ;
+
+let arr3 = arr1 ;
+console.log(arr3);
