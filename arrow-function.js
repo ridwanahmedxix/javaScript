1bb8f313e0bffbc66975f03145a37b25fe3e2078
ridwancodexr <ridwancodexr@gmail.com>
2025-10-14 12:10:49 +0600

    add

---
+++ arrow-function.js
@@ -0,0 +1,10 @@
+
+function sumNumber ( num1 , num2 ) {
+    return num1 + num2 ;
+}
+console.log(sumNumber(10,20));
+
+const sumNumber1 = (num1 , num2) => {
+    return num1 + num2 ;
+}
+console.log(sumNumber1( 20, 40 ));
