015d168c30acea120e83cc9736158f2603aba20b
ridwancodexr <ridwancodexr@gmail.com>
2025-10-15 14:32:22 +0600

    add

---
+++ Array-Slice-and-Splice-in-JavaScript.js
@@ -0,0 +1,5 @@
+
+const frinds = [ 'SUMON' , 'RAHIM' , 'TALHA' , 'SABBIR' ,'RIDWAN' , 'KAMRUL' ]  ;
+const friendName = frinds.splice( 1 , 4 , 'SADIA' , 'SAMIA' , 'NADIA' , 'SUMIAIYA' ) ;
+console.log(friendName);
+console.log(frinds);
