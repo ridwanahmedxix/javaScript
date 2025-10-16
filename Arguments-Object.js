41faca6f61cf6659592dbe9736beeef9dbd89db2
ridwancodexr <ridwancodexr@gmail.com>
2025-10-16 20:25:05 +0600

    add

--- Arguments-Object.js
+++ Arguments-Object.js
@@ -1,9 +1,14 @@
 
-function sum( ) {
-   let store = 0 ;
-  for( let i = 0 ; i < arguments.length ; i++ ) {
+function sum() {
+    if(arguments.length == 0) {
+      console.log("Aeguments Note Found");
+    } else{
+        for( let i = 0 ; i < arguments.length ; i++ ) {
    store = store + arguments[i]
   }
   console.log(store);
+
+    }
+       
 }
-sum( 2 , 6 , 9 , 7, 8) ;
+sum( 2 , 10) ;
