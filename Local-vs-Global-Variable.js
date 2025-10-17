3ea460164619f7d36d1735f7c557550d412492b7
ridwancodexr <ridwancodexr@gmail.com>
2025-10-17 07:48:51 +0600

    add

--- Local-vs-Global-Variable.js
+++ Local-vs-Global-Variable.js
@@ -3,7 +3,12 @@ let b = " Web Developer" //  Global Variable
 
 function func1() {
     let a = 'Ridwan Ahmed' ; // Local Variable 
-    
+    console.log(a);
 }
+function func2() {
+     let a = 'Rasel Ahmed' ;
+     console.log(a);
+}
+
 func1()
-console.log(b);
+func2()
