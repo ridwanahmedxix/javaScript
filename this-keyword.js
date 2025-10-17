f63a7131baf726dadee071b2c2a47b395085dbd2
ridwancodexr <ridwancodexr@gmail.com>
2025-10-17 18:55:11 +0600

    add

---
+++ this-keyword.js
@@ -0,0 +1,13 @@
+
+const user = {
+    firstName: 'Ridwan' ,
+    lastName: 'Ahmed' ,
+    message: function () {
+        console.log(` HELLO ! my device name ${product.productName} `);
+    }
+} ;
+const product = {
+    productName: ' I phone ' ,
+    age: 18
+}
+user.message() ;
