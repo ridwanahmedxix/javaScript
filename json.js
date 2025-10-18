dfd248e3054633383216ab67e590a7c23391c65e
ridwancodexr <ridwancodexr@gmail.com>
2025-10-18 06:29:24 +0600

    add

--- json.js
+++ json.js
@@ -1,5 +1,7 @@
-let text = '{ "employees" : [' +
-'{ "firstName":"John" , "lastName":"Doe" },' +
-'{ "firstName":"Anna" , "lastName":"Smith" },' +
-'{ "firstName":"Peter" , "lastName":"Jones" } ]}';
- console.log(JSON.parse(text) );
+
+const person = {
+    firstName: 'Ridwan' ,
+    lastName: 'Ahmed' ,
+    age: 18
+}
+console.log(JSON.stringify(person));
