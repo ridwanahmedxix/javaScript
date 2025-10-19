a7d8f1ebb8e630013a1ccc86b168a6926ec83bff
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 09:36:18 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -1,5 +1,12 @@
 
-var num1 = "20" ;
-var num2 = 30 ;
-num1 = parseFloat(num1)
-console.log(num1+num2);
+// var num1 = "20" ;
+// var num2 = 30 ;
+// num1 = parseFloat(num1)
+// console.log(num1+num2);
+
+var num3 = 100 ;
+var num4 = 200 ;
+num3 = "" + num3 ;
+num4 = "" + num4 ;
+var mot = num3 + num4 ;
+console.log(mot);
