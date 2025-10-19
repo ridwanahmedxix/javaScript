e8ec3a13a17139b37001daad9f05913c84d0315e
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 10:18:11 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -1,25 +1,5 @@
-var num1 = 100 ;
-var num2 = 50 ;
-var total = num1 - num2 ;
-console.log(total);
 
-
-var num3 = 5 ;
-var num4 = 10 ;
-var total1 = num3 * num4 ;
-console.log(total1);
-
-
-var num5 = 500 ;
-var num6 = 5 ;
-var total2 = num5 / num6 ;
-console.log(total2);
-
-var num5 = 80 ;
-var num6 = 70 ;
-var total2 = num5 % num6 ;
-console.log(total2);
-
-
-
-
+var totalMembers = 10 ;
+var voter = 2.7865236 ;
+var sum = Math.round(voter)
+console.log(sum);
