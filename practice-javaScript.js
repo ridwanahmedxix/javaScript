75de94dfb7154d2f2854e5787f6ebc6828cea677
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 10:59:39 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -1,5 +1,30 @@
 
-var totalMembers = 10 ;
-var voter = 7.48647865236 ;
-var sum = Math.floor(voter)
-console.log(sum);
+// var totalMembers = 10 ;
+// var voter = 7.48647865236 ;
+// var sum = Math.floor(voter)
+// console.log(sum);
+
+
+// var lotary = Math.random()
+// console.log(lotary);
+
+// var lotary = Math.random() * 20 ;
+// console.log(lotary);
+
+// var a = 2 ;
+// var b = 5 ;
+// if( a < b ) {
+//     console.log("Hello , This is True ");
+// }
+// else{
+//     console.log(" This is False");
+// }
+
+
+
+if( 10 > 1 ) {
+   console.log(" Amer mon bose na Gram a  ");
+}
+else{
+    console.log(" Taito ailam shohore  ");
+}
