8c4c08ae1d84289b1e3b15591cdaa7fe8a2f56b2
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 09:07:02 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -1,7 +1,5 @@
 
-var myName = " Ridwan Ahmed  " ;
-console.log( typeof myName);
-var numType = 100 ;
-console.log(typeof numType);
-var isMyGf = true ;
-console.log(typeof isMyGf);
+var cowPara = " A paragraph is a distinct section of writing, typically consisting of several sentences, that focuses on a single idea or topic. It is a way to organize prose, making written work easier to read by breaking it into digestible chunks. Key characteristics of a good paragraph include unity (all sentences relate to the topic), coherence (sentences are logically arranged), and development (the idea is adequately supported).   "
+
+console.log(cowPara.toUpperCase());
+
