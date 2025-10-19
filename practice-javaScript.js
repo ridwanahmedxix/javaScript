dd686c00da51d7ee4a6c2c03f3df7743dfae9a05
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 14:39:20 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -39,14 +39,47 @@
 
 
 
-var x = 5 ;
-var y = 5 ;
-if( x > y ) {
-    console.log("Boro bhai");
-}
-else if ( x >= y )  {
-   console.log(" Soman Bhai");
-}
-else{
-     console.log(" Soto Bhai");
-}
+// var x = 5 ;
+// var y = 5 ;
+// if( x > y ) {
+//     console.log("Boro bhai");
+// }
+// else if ( x >= y )  {
+//    console.log(" Soman Bhai");
+// }
+// else{
+//      console.log(" Soto Bhai");
+// }
+
+
+// var sum = 100 ;
+// switch(sum) {
+//     case 10 :
+//         console.log(" Ridwan Ahmed ");
+//         break ;
+//         case 5 :
+//             console.log(" Now , I am in 5");
+//             break ;
+//             case 15 :
+//                 console.log("Now , I am in 15");
+//                 break ;
+//                 default :
+//                 console.log(" Ami pailam na tumay ");
+// }
+
+// var sum = 10 ;
+// switch(sum) {
+//     case 10 :
+//         console.log(" Ridwan Ahmed ");
+        
+//         case 5 :
+//             console.log(" Now , I am in 5");
+            
+//             case 15 :
+//                 console.log("Now , I am in 15");
+// }
+
+
+
+
+
