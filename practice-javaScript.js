f0dee4c64195f6f146185f6cf1f8f20f4485fa2e
ridwancodexr <ridwancodexr@gmail.com>
2025-10-19 09:26:47 +0600

    add

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -1,7 +1,5 @@
 
-var cowPara = " A paragraph is a distinct section of writing, typically consisting of several sentences, that focuses on a single idea or topic. It is a way to organize prose, making written work easier to read by breaking it into digestible chunks. Key characteristics of a good paragraph include unity (all sentences relate to the topic), coherence (sentences are logically arranged), and development (the idea is adequately supported).   "
-
-console.log(cowPara.toUpperCase());
-
-var newPara = " A PARAGRAPH IS A DISTINCT SECTION OF WRITING, TYPICALLY CONSISTING OF SEVERAL SENTENCES, THAT FOCUSES ON A SINGLE IDEA OR TOPIC. IT IS A WAY TO ORGANIZE PROSE, MAKING WRITTEN WORK EASIER TO READ BY BREAKING IT INTO DIGESTIBLE CHUNKS. KEY CHARACTERISTICS OF A GOOD PARAGRAPH INCLUDE UNITY (ALL SENTENCES RELATE TO "
-console.log(newPara.toLocaleLowerCase());
+var num1 = "20" ;
+var num2 = 30 ;
+num1 = parseFloat(num1)
+console.log(num1+num2);
