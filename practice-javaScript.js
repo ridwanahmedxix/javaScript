1320ee8a7dbb24c030fdcafa05e7882aef2e02fa
ridwancodexr <ridwancodexr@gmail.com>
2025-10-20 06:27:32 +0600

    addddddd

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -191,10 +191,20 @@
 // }
 
 
+// let i = 0 ;
+// while( i <= 10 ) {
+//     console.log( i , " MY NAME IS ROCKSTAR RAKIB ");
+//     i++ ;
+// }
 
 
 
 
+// let i = 1 ;
+// while( i <= 6 ){
+//     console.log( i , "Web Developer");
+//     i++ ;
+// }
 
 
 
