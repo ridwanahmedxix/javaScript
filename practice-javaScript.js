6f0d9c31e0789870c989e40f5d822689c7230071
ridwancodexr <ridwancodexr@gmail.com>
2025-10-20 20:33:15 +0600

    addd

--- practice-javaScript.js
+++ practice-javaScript.js
@@ -372,6 +372,17 @@
  
 
 
+// let userOneValue = prompt(" Number One Data ") ;
+// let userTwoValue = prompt( " Number Two Data " ) ;
+
+// userOneValue = parseInt(userOneValue) ;
+// userTwoValue = parseInt(userTwoValue) ;
+
+
+// const Result = userOneValue + userTwoValue ;
+
+
+// document.write(  " Output Resut " + Result + " <br> " ) ;
 
 
 
