9f4f814c1dcd12deaf867af81ec1b93808b00e46
ridwancodexr <ridwancodexr@gmail.com>
2025-10-27 07:39:06 +0600

    add

--- Stack-Learner.js
+++ Stack-Learner.js
@@ -1,5 +1,91 @@
+// let a = 20 ;
+// let b = 30 ;
+// let c = a + b ;
+// console.log(" a " + "+" + " b " + " = " + c );
 
-let a = 10 ;
-let b = 25 ;
-let c = a + b ;
-console.log(c);
+
+
+// let a = 1000 ;
+// let b = true ;
+// let c = "Hello Bangladesh" ;
+// let d = `Ridwan` ;
+// let e 
+// let f = null ;
+
+// console.log( typeof a );
+// console.log( typeof b );
+// console.log( typeof c );
+// console.log( typeof d );
+// console.log( typeof d );
+// console.log( typeof e );
+// console.log( typeof f );
+
+// let n = 5 ;
+// console.log(++n);
+
+
+let n = 5 ;
+let m = (10) + ++n ;
+console.log(m);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
