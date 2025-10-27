46b9edc603197f7378fabd8266ea3401e8251ffa
ridwancodexr <ridwancodexr@gmail.com>
2025-10-27 08:39:12 +0600

    add

--- DOM API/JavaScript/first.js
+++ DOM API/JavaScript/first.js
@@ -1,5 +1,31 @@
-const result =  document.getElementsByTagName('h2') ;
-console.log(result);
-for( let i = 0 ; i < result.length ; i++){
-    console.log(result[i]);
-}
+// const result =  document.getElementsByTagName('h2') ;
+// console.log(result);
+// for( let i = 0 ; i < result.length ; i++){
+//     console.log(result[i]);
+// // }
+// console.log(document.getElementsByTagName('span'));
+
+
+// console.log(document.getElementsByTagName('li'));
+
+// console.log(document.getElementById('hello'));
+
+// const result = document.getElementById('hello') ;
+// console.log(result);
+
+// const result = document.getElementsByClassName('bd')
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
