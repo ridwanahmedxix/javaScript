4675dd975e13dab21ba11a1d1e8432534623a3a4
ridwancodexr <ridwancodexr@gmail.com>
2025-11-07 07:54:27 +0600

    JavaScript DOM - heading added

--- JavaScript DOM HABLU/index.js
+++ JavaScript DOM HABLU/index.js
@@ -4,5 +4,9 @@
 //  let paraTag1 = document.getElementsByClassName('paraTag1') [0 , 1 , 2] ;
 //  paraTag1.innerHTML = 'hello bro'
 
-let heading = document.getElementsByName('heading2') [0] ;
-heading.innerHTML = 'Hello'
+// let heading = document.getElementsByName('heading2') [0] ;
+// heading.innerHTML = 'Hello'
+
+
+  let Heading1 = document.getElementsByTagName('h1') [0] ;
+   Heading1.innerHTML = "Hello , This is Bangladesh"
