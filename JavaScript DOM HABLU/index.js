35d226d7e8195425c8ac4e9fac331690a53baff2
ridwancodexr <ridwancodexr@gmail.com>
2025-11-08 06:33:56 +0600

    create 3 li with js

--- JavaScript DOM HABLU/index.js
+++ JavaScript DOM HABLU/index.js
@@ -43,16 +43,31 @@
 // document.body.appendChild(div)
 
 
- let ul = document.createElement('ul') ;
- ul.id = "menu" ;
+//  let ul = document.createElement('ul') ;
+//  ul.id = "menu" ;
 
-let li1 = document.createElement('li')
+// let li1 = document.createElement('li')
+// li1.textContent = "Home" ;
+// ul.appendChild(li1) ;
+
+// document.body.appendChild(ul)
+
+let ul = document.createElement('ul') ;
+ul.id = "menu" ;
+
+let li1 = document.createElement('li') ;
 li1.textContent = "Home" ;
 ul.appendChild(li1) ;
 
+let li2 = document.createElement('li') ;
+li2.textContent = "About" ;
+ul.appendChild(li2) ;
+
+let li3 = document.createElement('li') ;
+li3.textContent = "Contact" ;
+ul.appendChild(li3)
+
 document.body.appendChild(ul)
-
-
 
 
 
