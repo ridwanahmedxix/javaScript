967e58ec439cd9ef39d2217bd72ce8801956f4ce
ridwancodexr <ridwancodexr@gmail.com>
2025-11-08 06:26:59 +0600

    create li with js

--- JavaScript DOM HABLU/index.js
+++ JavaScript DOM HABLU/index.js
@@ -46,6 +46,12 @@
  let ul = document.createElement('ul') ;
  ul.id = "menu" ;
 
+let li1 = document.createElement('li')
+li1.textContent = "Home" ;
+ul.appendChild(li1) ;
+
+document.body.appendChild(ul)
+
 
 
 
