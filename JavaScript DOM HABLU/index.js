8ba7486c0ff33fcc119f6e680a1b9baae9cf61cf
ridwancodexr <ridwancodexr@gmail.com>
2025-11-09 18:05:33 +0600

    insertAdjacentHTML

--- JavaScript DOM HABLU/index.js
+++ JavaScript DOM HABLU/index.js
@@ -186,8 +186,10 @@
 
 
 let menu = document.querySelector("#menu") ;
-menu.insertAdjacentHTML("beforebegin" , "<h3> My name is Ridwan </h3>")
-
+menu.insertAdjacentHTML("beforebegin" , "<h3> beforebegin </h3>")
+menu.insertAdjacentHTML("afterbegin" , "<h3> afterbegin </h3>")
+menu.insertAdjacentHTML("beforeend" ,  "<h3> beforeend </h3>")
+menu.insertAdjacentHTML("afterend" , "<h3> afterend </h3>")
 
 
 
