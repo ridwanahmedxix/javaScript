2164be51d8fbfaab45b75b0355bb6f83968eaa99
ridwancodexr <ridwancodexr@gmail.com>
2025-11-13 14:47:55 +0600

    RadioButton create with js

--- JavaScript DOM HABLU/index.js
+++ JavaScript DOM HABLU/index.js
@@ -372,3 +372,31 @@
 //     const FromControl = input.parentElemnt
 //     FromControl.className = "from-control success"
 // }
+
+
+const group = document.querySelector(".group") ;
+const output = document.querySelector(".output") ;
+
+const sizes = ["XS" , "S" , "M" , "L" , "XL" , "XXL"];
+
+group.innerHTML = sizes.map((size) => 
+     ` <div>   
+
+        <input type="radio" id="${size}" value="${size}" name="size">
+        <label for="${size}"></label>
+
+      </div> `
+).join(" ")
+
+const RadioButton = document.querySelectorAll("input")
+
+for( const radiobtn of RadioButton){
+    radiobtn.addEventListener("change" , ShowOutPut )
+}
+
+function ShowOutPut(e){
+    console.log(e);
+    if(this.checked){
+        document.querySelector(".output").innerHTML = `You Selected ${this.value} `
+    }
+}
