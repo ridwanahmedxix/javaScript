260ee63d2a9a1f1fe6cc2b2dc7d00fb94e66d552
ridwancodexr <ridwancodexr@gmail.com>
2025-11-14 07:21:39 +0600

    all commit

--- JavaScript-DOM-HABLU/index.js
+++ JavaScript-DOM-HABLU/index.js
@@ -444,39 +444,39 @@
 
 
 
-const Framework = document.querySelector("#Lang") ;
-const btnAdd = document.querySelector("#btnAdd") ;
-const list = document.querySelector("#list") ;
-const btnRemove = document.querySelector("#btnRemove") ;
+// const Framework = document.querySelector("#Lang") ;
+// const btnAdd = document.querySelector("#btnAdd") ;
+// const list = document.querySelector("#list") ;
+// const btnRemove = document.querySelector("#btnRemove") ;
 
-btnAdd.onclick = ((e) => {
-e.preventDefault() ;
+// btnAdd.onclick = ((e) => {
+// e.preventDefault() ;
 
-if(Framework.value == ""){
-    alert("Bro ?! Brain Use")
-}
+// if(Framework.value == ""){
+//     alert("Bro ?! Brain Use")
+// }
 
-const option = new Option(Framework.value) ;
+// const option = new Option(Framework.value) ;
 
-list.add(option) ;
-Framework.value = "" ;
+// list.add(option) ;
+// Framework.value = "" ;
 
-})
+// })
 
-btnRemove.onclick = (e) => {
-e.preventDefault() ;
+// btnRemove.onclick = (e) => {
+// e.preventDefault() ;
 
-let selected = [] ;
-for( let i = 0 ; i < list.options.length ; i++){
-    selected[i] = list.options[i].selected ;
-}
+// let selected = [] ;
+// for( let i = 0 ; i < list.options.length ; i++){
+//     selected[i] = list.options[i].selected ;
+// }
 
-let index = list.options.length ;
+// let index = list.options.length ;
 
-while(index--){
-  if(selected[index]){
-    list.remove(index)
-  }
-}
+// while(index--){
+//   if(selected[index]){
+//     list.remove(index)
+//   }
+// }
 
-}
+// }
